4b88065976291776ad050632dbd4169aaadd6028
FosterToster <dimatsuv@gmail.com>
2022-08-15 21:31:57 +0700

    thiserror

--- src/errors.rs
+++ src/errors.rs
@@ -1,50 +1,22 @@
-use std::error::Error;
-use std::fmt::Display;
+use thiserror::Error;
 
-#[derive(Debug)]
+#[derive(Error, Debug)]
 pub enum StorageError {
+    #[error("sth went wrong in storage")]
     StorageError,
 }
 
-impl Display for StorageError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Storage error")
-    }
-}
-
-impl Error for StorageError {}
-
-#[derive(Debug)]
+#[derive(Error, Debug)]
 pub enum SmartHomeError {
+    #[error("Not found: {0}")]
     NotFound(String),
+
+    #[error("Not unique: {0}")]
     NotUnique(String),
+
+    #[error("Not empty: {0}")]
     NotEmpty(String),
-    StorageError(String),
-}
 
-impl Display for SmartHomeError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::NotFound(text) => {
-                write!(f, "Item is not found: {}", text)
-            }
-            Self::NotUnique(text) => {
-                write!(f, "Item is not uniqe: {}", text)
-            }
-            Self::StorageError(text) => {
-                write!(f, "Storage returned an error: {}", text)
-            }
-            Self::NotEmpty(text) => {
-                write!(f, "Storage returned an error: {}", text)
-            }
-        }
-    }
-}
-
-impl Error for SmartHomeError {}
-
-impl From<StorageError> for SmartHomeError {
-    fn from(_: StorageError) -> Self {
-        Self::StorageError("Something went wrong".into())
-    }
+    #[error("Storage error: {0}")]
+    StorageError(#[from] StorageError),
 }
